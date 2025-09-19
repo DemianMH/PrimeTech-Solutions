f793c49fdda1e89cdf0e097ea9fe920fbaebfbad
DemianMH <za19011250@zapopan.tecmm.edu.mx>
2025-09-19 10:48:33 -0600

    Pagina Finalizada

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,35 @@ import MainLayoutWrapper from "@/components/ui/main-layout-wrapper";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+
+  metadataBase: new URL('https://primetech-solutions.netlify.app/'),
+  
   title: "PrimeTech Solutions",
-  description: "Soluciones Integrales 360",
+  description: "Soluciones Integrales 360: Desarrollo de Software, Soporte IT y Diseño Gráfico.",
+  
+
+  openGraph: {
+    title: "PrimeTech Solutions",
+    description: "Soluciones Integrales 360",
+    images: [
+      {
+        url: '/logo-text-white.png', 
+        width: 1200, 
+        height: 630, 
+        alt: 'Logo de PrimeTech Solutions',
+      },
+    ],
+    locale: 'es_MX',
+    type: 'website',
+  },
+
+ 
+  twitter: {
+    card: 'summary_large_image',
+    title: 'PrimeTech Solutions',
+    description: 'Soluciones Integrales 360',
+    images: ['/logo-text-white.png'], 
+  },
 };
 
 export default function RootLayout({
